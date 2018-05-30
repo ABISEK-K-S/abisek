a5d118d00bcaa9d15f07e7fb32b35ff96bc352e2
ABISEK-K-S <39756233+ABISEK-K-S@users.noreply.github.com>
2018-05-30 12:14:47 +0530

    Initial commit

---
+++ abisek/app/src/main/java/audio/example/org/abisek/MainActivity.java
@@ -0,0 +1,13 @@
+package audio.example.org.abisek;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
